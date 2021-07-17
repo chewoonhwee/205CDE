769b85b7b332a8e04f95c35d10143ce34e72961d
chewoonhwee <oonhwee217@gmail.com>
2021-07-17 15:38:31 +0800

    unuse

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    //owl banner slider
-    $("#bannerSlider .myCarousel").carousel({
-        item: 1,
-        loop: true,
-        autoplay: true,
-        nav:true
-    });
-});
